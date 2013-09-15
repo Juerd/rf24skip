d49556fa03144b2005761acd02e44830fc30d1f2
Juerd Waalboer <juerd@tnx.nl>
2013-09-16 01:17:59 +0200

    Remove unneeded stuff.
    This was copy/pasted from rf24server, but wasn't needed for this
    simple client at all.

--- rf24skip.ino
+++ rf24skip.ino
@@ -5,23 +5,8 @@
 // NB: This sketch assumes 32 bit addresses
 
 static long int address = 0x66996699L;  // So that's 0x0066996699
-const int payload = 16;  // 32 is supported but not very reliable
 
 RF24 rf(/*ce*/ 8, /*cs*/ 10);
-
-static union {
-    unsigned char buf[36];
-    struct {
-        uint32_t address;
-        unsigned char message[payload];
-    } packet;
-} in;
-
-unsigned char hexdigit(byte c) {
-    if (c >= '0' && c <= '9') return c - '0';
-    if (c >= 'a' && c <= 'f') return c - 'a' + 10;
-    if (c >= 'A' && c <= 'F') return c - 'A' + 10;
-}
 
 void setup() {
     pinMode(9, OUTPUT);
