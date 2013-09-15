b420746b710d31b233046a75ab726fd7b8afea80
Juerd Waalboer <juerd@tnx.nl>
2013-09-16 00:07:05 +0200

    Change minimum interval

--- skip.pl
+++ skip.pl
@@ -3,7 +3,7 @@ use strict;
 use Time::HiRes qw(time);
 use LWP::Simple qw(get);
 
-my $minimum_time = 1;
+my $minimum_time = .5;
 my $url = 'http://jukebox:9000/Classic/status_header.html?p0=playlist&p1=jump&p2=%2B1&player=be%3Ae0%3Ae6%3A04%3A46%3A38';
 
 my $dev = (glob "/dev/ttyUSB*")[0];
