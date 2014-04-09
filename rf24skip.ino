269a7e838c28e22deaee4438227ecf709384eb1c
Juerd Waalboer <juerd@tnx.nl>
2014-04-09 18:30:24 +0200

    Shake == shuffle
    Oh, and some stuff that was added before but not commited.

--- rf24skip.ino
+++ rf24skip.ino
@@ -1,16 +1,21 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <AcceleroMMA7361.h>
 
 // NB: This sketch assumes 32 bit addresses
 
 static long int address = 0x66996699L;  // So that's 0x0066996699
 const int tries = 10;
 
+AcceleroMMA7361 acc;
 RF24 rf(/*ce*/ 8, /*cs*/ 10);
 
 void setup() {
     Serial.begin(115200);
+    acc.begin(/*sleep*/ 4, /*test*/ 6, /*0G*/ 8, /*Gselect*/ 5, /*xyz*/A0,A1,A2);
+    acc.setSensitivity(LOW);
+
     rf.begin();
     rf.setRetries(15, 15);
     rf.enableDynamicPayloads();
@@ -21,12 +26,38 @@ void setup() {
 }
 
 void loop() {
+    static int oldx = 0, oldy = 0, oldz = 0;
+    static unsigned long oldt;
+    int x = acc.getXAccel();
+    int y = acc.getXAccel();
+    int z = acc.getXAccel();
+    unsigned long t = millis();
+    static bool firstloop = true;
+    static unsigned int shakiness = 0;
+    
+    int dx = x - oldx, dy = y - oldy, dz = z - oldz, dt = t - oldt;
+    oldx = x; oldy = y; oldz = z; oldt = t;
+    
+    if (firstloop) {
+      firstloop = false;
+      return;
+    }
+    
     if (millis() >= 2500) {
-        char buf[6] = "\x04STOP";
+        char SHUF[6] = "\x04SHUF";
+        char STOP[6] = "\x04STOP";
+        
         for (int i = 0; i < tries; i++)
-            rf.write(&buf, 5);
+            rf.write(shakiness < 900 ? &STOP : &SHUF, 5);
+        
         for (;;);  // halt
     }
+    
+    float shock2 = (float) (dx*dx* + dy*dy + dz*dz)/dt;
+    shakiness += abs(shock2) / 100;
+    Serial.println(shakiness);
+          
+    delay(10);
 
 }
 
--- skip.pl
+++ skip.pl
@@ -2,11 +2,31 @@
 use strict;
 use Time::HiRes qw(time);
 use LWP::Simple qw(get);
+use POSIX qw(strftime tcflush TCIFLUSH);
+
+use IO::Socket::INET;
+sub ledbanner {
+	my $socket = IO::Socket::INET->new(qw/PeerAddr 10.42.76.66 PeerPort 12345 Proto udp/)
+		or warn $!;
+	$socket->send(shift);
+	close $socket;
+}
+
 
 my $minimum_time = .5;
+
 my %url = (
 	SKIP => 'http://jukebox:9000/Classic/status_header.html?p0=playlist&p1=jump&p2=%2B1&player=be%3Ae0%3Ae6%3A04%3A46%3A38',
 	STOP => 'http://jukebox:9000/Classic/status_header.html?p0=stop&player=be%3Ae0%3Ae6%3A04%3A46%3A38',
+	SHUF => 'http://jukebox:9000/Classic/plugins/RandomPlay/mix.html?type=track&player=be%3Ae0%3Ae6%3A04%3A46%3A38&addOnly=0',
+	NOMZ => sub {
+		ledbanner("NOMZ");
+	},
+	CO_2 => sub {
+		my ($co2) = unpack "n", shift;
+		print $co2, "\n";;
+		ledbanner($co2 > 1600 ? "!!sticky!!CO2 HIGH" : "!!reset!!CO2 HIGH");
+	}
 );
 
 my $dev = (glob "/dev/ttyUSB*")[0];
@@ -24,11 +44,20 @@ while (1) {
     while (<$fh>) {
         s/[\r\n]//g;
         my $msg = pack "H*", $_;
-	print "$msg\n";
-        if (exists $url{$msg}) {
-            next if $prev{$msg} and $prev{$msg} > (time() - $minimum_time);
-            get $url{$msg};
-            $prev{$msg} = time();
+	my $type = substr $msg, 0, 4;
+        my $data = substr $msg, 4;
+
+        if (exists $url{$type}) {
+           next if $prev{$type} and $prev{$type} > (time() - $minimum_time);
+	    print "$type\n";
+            if (ref $url{$type}) {
+		$url{$type}->($data);
+	    } else {
+   		get $url{$type};
+	    }
+            $prev{$type} = time();
+        } else {
+            print "Unknown: $msg\n";
         }
     }
 }
