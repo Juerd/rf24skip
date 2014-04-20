72c0542c66650b992902605f6815faae2a78781c
root <root@squeezebox-klusbunker.(none)>
2014-04-20 21:14:58 +0000

    Use perl's MQTT library instead of shell-escape

--- skip.pl
+++ skip.pl
@@ -5,27 +5,60 @@ use LWP::Simple qw(get);
 use POSIX qw(strftime tcflush TCIFLUSH);
 
 use IO::Socket;
-
 use IO::Socket::INET;
+
+use Net::MQTT::Constants;
+use Net::MQTT::Message;
+use IO::Select;
+
+my $verbose = 0;
+my $stdin_buf = '';
+my $sock_buf = '';
+my $mid = 1;
+my $next_ping;
+my $got_ping_response = 1;
+# Defaults
+my $mqtthost = "127.0.0.1";
+my $mqttport = "1883";
+my $keep_alive_timer = 60;
+
+our $mqttsocket = IO::Socket::INET->new(PeerAddr => $mqtthost.':'.$mqttport,
+                        Timeout => $keep_alive_timer,
+                       ) or die "Socket connect failed: $!\n";
+
 sub ledbanner {
-    my $socket = IO::Socket::INET->new(qw/PeerAddr 10.42.76.66 PeerPort 12345 Proto udp/)
+    my $ledsocket = IO::Socket::INET->new(qw/PeerAddr 10.42.76.66 PeerPort 12345 Proto udp/)
         or warn $!;
-    $socket->send(shift);
-    close $socket;
+    $ledsocket->send(shift);
+    close $ledsocket;
 }
 
-# MQTT Message, with persistence
+# Send an MQTT (low-level) message
+sub send_message {
+  my $socket = shift;
+  my $msg = Net::MQTT::Message->new(@_);
+  print 'Sending: ', $msg->string, "\n" if ($verbose >= 2);
+  $msg = $msg->bytes;
+  syswrite $socket, $msg, length $msg;
+  print dump_string($msg, 'Sent: '), "\n\n" if ($verbose >= 3);
+  $next_ping = Time::HiRes::time + $keep_alive_timer;
+}
+
+# MQTT Message - high-level
 sub mqtt {
-    my ($topic, $message, $persistent) = @_;
-    my @persistent = $persistent ? "-r" : ();
+    my ($topic, $message, $retain) = @_;
 
-    system mosquitto_pub => (
-        @persistent,
-        "-h" => "127.0.0.1",
-        "-t" => "/revspace/$topic",
-        "-m" => "$message"
-    );
+    send_message($mqttsocket,
+        message_type => MQTT_PUBLISH,
+        retain => $retain,
+        topic => "/revspace/" . $topic,
+        message => $message);
 }
+
+
+# Connect to MQTT broker
+send_message($mqttsocket, message_type => MQTT_CONNECT,
+             keep_alive_timer => $keep_alive_timer);
 
 my $minimum_time = .5;
 
