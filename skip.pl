faf929ee5afd492347ce82c7bb8d3844c56b60a4
root <root@squeezebox-klusbunker.(none)>
2014-04-20 18:05:53 +0000

    mqtt, ledbanner, cleanups

--- skip.pl
+++ skip.pl
@@ -2,11 +2,46 @@
 use strict;
 use Time::HiRes qw(time);
 use LWP::Simple qw(get);
+use POSIX qw(strftime tcflush TCIFLUSH);
+
+use IO::Socket;
+
+use IO::Socket::INET;
+sub ledbanner {
+    my $socket = IO::Socket::INET->new(qw/PeerAddr 10.42.76.66 PeerPort 12345 Proto udp/)
+        or warn $!;
+    $socket->send(shift);
+    close $socket;
+}
+
+# MQTT Message, with persistence
+sub mqtt {
+    my ($topic, $message, $persistent) = @_;
+    my @persistent = $persistent ? "-r" : ();
+
+    system mosquitto_pub => (
+        @persistent,
+        "-h" => "127.0.0.1",
+        "-t" => "/revspace/$topic",
+        "-m" => "$message"
+    );
+}
 
 my $minimum_time = .5;
+
 my %url = (
-	SKIP => 'http://jukebox:9000/Classic/status_header.html?p0=playlist&p1=jump&p2=%2B1&player=be%3Ae0%3Ae6%3A04%3A46%3A38',
-	STOP => 'http://jukebox:9000/Classic/status_header.html?p0=stop&player=be%3Ae0%3Ae6%3A04%3A46%3A38',
+    SKIP => 'http://jukebox:9000/Classic/status_header.html?p0=playlist&p1=jump&p2=%2B1&player=be%3Ae0%3Ae6%3A04%3A46%3A38',
+    STOP => 'http://jukebox:9000/Classic/status_header.html?p0=stop&player=be%3Ae0%3Ae6%3A04%3A46%3A38',
+    SHUF => 'http://jukebox:9000/Classic/plugins/RandomPlay/mix.html?type=track&player=be%3Ae0%3Ae6%3A04%3A46%3A38&addOnly=0',
+    NOMZ => sub {
+        ledbanner("NOMZ");
+    },
+    CO_2 => sub {
+        my ($co2) = unpack "n", shift;
+        print $co2, "\n";;
+        mqtt( "sensors/co2", $co2, 1 );
+        ledbanner($co2 > 1600 ? "!!sticky!!CO2 HIGH" : "!!reset!!CO2 HIGH");
+    }
 );
 
 my $dev = (glob "/dev/ttyUSB*")[0];
@@ -24,11 +59,30 @@ while (1) {
     while (<$fh>) {
         s/[\r\n]//g;
         my $msg = pack "H*", $_;
-	print "$msg\n";
-        if (exists $url{$msg}) {
-            next if $prev{$msg} and $prev{$msg} > (time() - $minimum_time);
-            get $url{$msg};
-            $prev{$msg} = time();
+        my $type = substr $msg, 0, 4;
+        my $data = substr $msg, 4;
+
+        if (not exists $url{$type}) {
+            print "Unknown: $msg\n";
+            next;
         }
+
+        next if $prev{$type} and $prev{$type} > (time() - $minimum_time);
+
+        mqtt( "button/skip", "Skip pressed") if ( $type eq "SKIP" );
+        mqtt( "button/nomz", "NOMZ pressed") if ( $type eq "NOMZ" );
+        mqtt( "button/shuffle", "Shuffle pressed") if ( $type eq "SHUF" );
+        mqtt( "button/stop", "Stop pressed") if ( $type eq "STOP" );
+
+        print "$type\n";
+
+        if (ref $url{$type}) {
+            $url{$type}->($data);
+        } else {
+            get $url{$type};
+        }
+        $prev{$type} = time();
     }
+    sleep 1;
 }
+
